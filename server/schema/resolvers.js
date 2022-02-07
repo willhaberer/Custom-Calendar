4a90e36d04f9d1c1d043d7c467aa7a3bd7e8fcbf
Will Haberer <willhaberer@gmail.com>
2022-02-07 09:28:29 -0500

    created resolvers for user routes

---
+++ server/schema/resolvers.js
@@ -0,0 +1,51 @@
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find();
+    },
+
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("spotifysongs");
+      }
+      throw new AuthenticationError("Not logged in");
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    removeUser: async (parent, { userId }, context) => {
+      if (context.user) {
+        const deletedUser = User.findOneAndDelete({ _id: userId });
+        return deletedUser;
+      }
+      throw new AuthenticationError("Incorrect credentials!");
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials!");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials!");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
