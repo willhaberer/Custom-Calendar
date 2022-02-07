816640c7e68fef571211cd6a85bb671607cc15b9
Will Haberer <willhaberer@gmail.com>
2022-02-07 09:41:36 -0500

    created base home.js

---
+++ client/src/utils/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div id="mainContainer">
+      <h1>Custom Calendar</h1>
+    </div>
+  );
+}
+
+export default Home;
