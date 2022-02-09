edcfb6793ff455c3376f14e0bd07daf92a21bca3
Will Haberer <willhaberer@gmail.com>
2022-02-09 18:39:19 -0500

    created navbar.js

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import { GET_ME } from "../utils/queries";
+import "../styles/Navbar.css";
+
+import Auth from "../utils/auth";
+
+const Navbar = () => {
+  const { data } = useQuery(GET_ME);
+  const userData = data?.me || {};
+
+  const logoutUser = async (event) => {
+    event.preventDefault();
+    console.log("logout clicked");
+    Auth.logout();
+  };
+
+  if (!userData?.username) {
+    return (
+      <div id="navbar">
+        <Link id="link" style={{ textDecoration: "none" }} to="/">
+          <img
+            id="logo"
+            className="image"
+            src={logo}
+            alt="logo"
+            height="15%"
+            width="15%"
+          />
+        </Link>
+        <Link id="link" to="/login">
+          <h3 id="loginLink">Login</h3>
+        </Link>
+      </div>
+    );
+  }
+
+  return (
+    <div id="navbar">
+      <a>
+        <h3 id="logout" onClick={logoutUser}>
+          Logout
+        </h3>
+      </a>
+    </div>
+  );
+};
+
+export default Navbar;
