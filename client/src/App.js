c456851650f3a3ee64ecc1df1d982ebab035a408
Will Haberer <willhaberer@gmail.com>
2022-02-21 13:06:14 -0500

    added profile to router

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Create from "./pages/Create";
+import Profile from "./pages/Profile";
 
 const httpLink = createHttpLink({
   uri: "/graphql",
@@ -46,6 +47,7 @@ function App() {
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/signup" element={<Signup />} />
             <Route exact path="/create" element={<Create />} />
+            <Route exact path="/profile" element={<Profile />} />
           </Routes>
         </div>
       </BrowserRouter>
