67f9e7b4ea24a8067e9ac1f0570ee73710606543
Will Haberer <willhaberer@gmail.com>
2022-03-15 20:54:28 -0400

    each calendar has a delete button

--- client/src/pages/MyCals.js
+++ client/src/pages/MyCals.js
@@ -1,14 +1,21 @@
-import { useQuery } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 
 import { GET_ME } from "../utils/queries";
+import { REMOVE_CALENDAR } from "../utils/mutations";
 
 import "../styles/Profile.css";
 
 const MyCals = () => {
   const { data } = useQuery(GET_ME);
+  const [removeCalendar, { error }] = useMutation(REMOVE_CALENDAR);
 
   const userData = data?.me || {};
   console.log(userData);
+
+  const deleteCalendar = (event) => {
+    event.preventDefault();
+    console.log("remove calendar");
+  };
 
   if (!userData?.username) {
     return (
@@ -23,7 +30,14 @@ const MyCals = () => {
       <h1>MyCals</h1>
       <h1>Welcome Back {userData.username}!</h1>
       {userData.calendarList.map((calendar, c) => {
-        return <h3>Calendar: {calendar.name}</h3>;
+        return (
+          <>
+            <h3>Calendar: {calendar.name}</h3>
+            <button id="deleteCalBt" onClick={deleteCalendar}>
+              Delete
+            </button>
+          </>
+        );
       })}
     </div>
   );
